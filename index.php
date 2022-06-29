71338551d94dee54e4f5bad1b087b7e0fd3d43b3
Davide <franc.davi00@gmail.com>
2022-06-29 16:31:09 +0200

    milestone 1

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+    include __DIR__ . '/data.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+    foreach($dischi as $disco) { ?>
+        <div>
+            <div><img src="<?= $disco['poster'] ?>"/></div>
+            <div><?= $disco['title'] ?></div>
+            <div><?= $disco['author'] ?></div>
+            <div><?= $disco['year'] ?></div>
+        </div>
+        <?
+    } ?>
+</body>
+</html>
